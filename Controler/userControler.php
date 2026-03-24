c43056c0328811d587bdd0db902e6fb6b7a07451
izaantorrico <torricoizan@gmail.com>
2026-03-24 10:58:47 +0100

    actualizacioon

--- Controler/userControler.php
+++ Controler/userControler.php
@@ -1,57 +1,89 @@
 <?php
+session_start();
+If($_SERVER['REQUEST_METHOD'] == "POST"){
+    $user = new userControler();
+    //vemos que boton es el que presionar el usuario
+    if(isset($_POST["login"])){
+        echo "<p>login button clicked.</p>";
+        $user->login();
+    }
+    if(isset($_POST["logout"])){
+        echo "<p>logout button clicked.</p>";
+        $user->logout();
+    }
+    if(isset($_POST["register"])){
+        echo "<p>register button clicked.</p>";
+        $user->register();
+    }
 
+}
 
 
 
 //definimos clase
-class userControler{
+class userControler
+{
     //definimos atributos
 
     private $conn;
-    public function__construct(){
+    public function __construct()
+    {
+
+        $usuario = "root";
+        $password = "";
+
+        $host = "localhost";
+        $base_datos = "mi_base_datos";
+        //conexion creada
+        $this->conn = new mysqli($host, $usuario, $password, $base_datos);
 
 
-$host = "localhost";
-$usuario = "root";
-$password = "";
-$host = "localhost";
-$host = "localhost";
-$base_datos = "mi_base_datos";
-//conexion creada
-$conexion = new mysqli($host, $usuario, $password, $base_datos);
+        //verificamos si la sesion es correcta
+        // if($this->conn -> conect_error){
+        //     die("Error de conexion: " . $this->conn->connect_error);
+        // }
+        // echo "Conexion realizada";
 
+        //charset establecido
+        //$conexion -> set_charset("utf8mb4");
+        $this->conn->set_charset("utf8mb4");
 
-//verificamos si la sesion es correcta
-if($conexion -> conect_error){
-    die("Error de conexion: " . $conexion->connect_error);
-}
-echo "Conexion realizada";
+        //finalizar conexion
 
-//charset establecido
-$conexion -> set_charset("utf8mb4");
-
-//finalizar conexion
-$conexion -> close();
-
+        $this->conn->close();
+    }
 
     //metodos
-    public function login(){
+    public function login() {
+        $sql = "Select usuario, password";
+        $stmt = $this->conn->prepare($sql);
+        $usuario = "root";
+        $password = "";
+        $stmt ->bind_param("s",$usuario,"i",$password);
+
+        //ejecutar consulta
+        $stmt ->execute();
+
+        //resultados
+        $resultado = $stmt->get_result();
+        while($fila = $resultado ->fetch_assoc()){
+            echo"Usuario: " . $fila['usuario'] . 
+        }
+
+
 
     }
 
-    public function logout(){
 
-    }
 
-    public function register(){
+    public function logout() {}
 
-    }
+    public function register() {}
 
 
 
 
 
-//conexion con base de datos
+    //conexion con base de datos
 
-    }
-?>
+}
