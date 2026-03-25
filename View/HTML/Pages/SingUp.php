c151a09e86e93a5df736e937411368dfcf35ba55
alexBarrera457 <salexbarrera@gmail.com>
2026-03-25 10:44:59 +0100

    Registro Usuario

--- View/HTML/Pages/SingUp.php
+++ View/HTML/Pages/SingUp.php
@@ -1,20 +1,10 @@
 <?php
 session_start();
 
-$error = "";
+$error = $_SESSION['register_error'] ?? '';
+unset($_SESSION['register_error']);
 
-/*if(isset($_POST['Name'])) {
-    $_SESSION['name'] = $_POST['Name'];
-    $_SESSION['surname'] = $_POST['Surname'];
-    $_SESSION['email'] = $_POST['Email'];
-    $_SESSION['passwd'] = $_POST['Passwd'];
-    $_SESSION['repasswd'] = $_POST['Repasswd'];
 
-    register();
-    header("Location: HomeMenu.php");
-
-}
-//*/
 ?>
  
 <!DOCTYPE html>
@@ -24,20 +14,14 @@ $error = "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../CSS/Styles/SingUp.css" />
     <link rel="stylesheet" href="../../CSS/Global/global.css">
-    <title>Registrarse</title>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
+    <title>Registro Usuario</title>
 </head>
 <body>
  
     <header>
         <div class="header_web">
             <div class="logo">
-                <img src="../../Assets/Logo1.png">
+                <img src="../../Assets/Logo1.png" alt="Logo">
             </div>
             <div class="login_but">
                 <a href="../../HTML/Pages/Login.php"><input type="button" value="Iniciar sesión"></a>
@@ -48,8 +32,15 @@ $error = "";
     <div class = "fondo">
         <div class="recuadro_registro">
             <div class="titulo_registro">
-                <h1>Registro</h1>
+                <h1>Registro Usuario</h1>
             </div>
+
+                <?php if (!empty($error)): ?>
+                <div class="error_msg">
+                    <p><?= htmlspecialchars($error) ?></p>
+                </div>
+            <?php endif; ?>
+
             <div class="botones_form">
                 <form action="/../../Controler/userControler.php" method="post">
                     Nombre <br><input type="text" name="Name" required><br>
@@ -58,7 +49,8 @@ $error = "";
                     Correo electrónico <br><input type="email" name="Email" required><br>
                     Contraseña <br><input type="text" name="Passwd" required><br>
                     Repetir contaseña <br><input type="text" name="Reppasswd" required><br>
-                    <br><input type="submit" value="Registrarse">
+                    <input type="hidden" name="rol" value="usuario">
+                    <br><input type="submit" value="Registrarse" name="register">
                 </form>    
             </div>                          
         </div>
